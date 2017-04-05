14b932ec92f01a41aa89ff54ed492982be344137
Andrew York <andrew.g.york+github@gmail.com>
2017-04-05 11:51:07 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# rescan_line_sted
+# Line-rescanned STED microscopy is gentler and faster than point-descanned STED microscopy
+
+A scientific publication, describing a way to improve microscopy. This repository hosts everything you need to reproduce my results. Read the publication here:
+https://andrewgyork.github.io/rescan_line_sted
+
+This repository is intended to hold a (mostly) self-contained scientific publication. It contains:
+
+* HTML, javascript, CSS, and small images (`index.html`, `appendix.html`, and the `javascript`, `stylesheets`, and `images` directories)
+* A (limited) PDF version of the publication; note that this does not include animated/interactive figures.
+* Code which produces figures (in the `figure_code` directory). You can run and modify this code to verify and explore my results. See `figure_code/README.md` for instructions how to run the figure generation code.
+
+Some files aren't suitable for version control, (e.g. raw data, large numbers of pre-rendered figure images, etc). These are stored in a sister repository:
+
+https://github.com/AndrewGYork/rescan_line_sted_data
+
+Hopefully this makes it easier to manage big, non-text files without risking the faithful version control of small, important text-based files stored here.
+
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
---
+++ javascript/update_figures.js
@@ -0,0 +1,51 @@
+// All figures interactively loads static images.
+// Check if our big images are available locally or remotely:
+var big_image_directory = "./../big_images";
+var img = new Image();
+img.onerror = function() {
+  window.big_image_directory = "https://andrewgyork.github.io/rescan_line_sted_data";
+  img.src = big_image_directory + "/figure_1/point_1p00exc_9p00dep_008samps_001pulses.svg"
+  img.onerror = ""
+}
+img.onload = function() {
+  console.log("Loading interactive images from: " + big_image_directory)
+}
+img.src = big_image_directory + "/figure_1/point_1p00exc_9p00dep_008samps_001pulses.svg"
+
+// Now that we know where our images are, we can load them:
+function update_figure_1() {
+  var exc = document.getElementById("Figure_1_excitation").value;
+  var dep = document.getElementById("Figure_1_depletion").value;
+  var smp = document.getElementById("Figure_1_samples").value;
+  var pul = document.getElementById("Figure_1_pulses").value;
+  var shp = document.getElementById("Figure_1_shape").value;
+  var filename = big_image_directory + "/figure_1/" + shp + exc + dep + smp + pul + ".svg";
+  var image = document.getElementById("Figure_1_image");
+  image.src = filename;
+}
+
+function update_figure_2() {
+  var res = document.getElementById("Figure_2_resolution").value;
+  var img = document.getElementById("Figure_2_image").value;
+  var scn = document.getElementById("Figure_2_scan_type").value;
+  var filename = big_image_directory + "/figure_2/figure_2_" + img + res + scn;
+  var gif = document.getElementById("Figure_2_gif");
+  var svg = document.getElementById("Figure_2_svg");
+  gif.src = filename.concat(".gif");
+  svg.src = filename.concat(".svg");
+}
+
+function update_figure_3() {
+  var mth = document.getElementById("Figure_3_imaging_method").value;
+  var res = document.getElementById("Figure_3_resolution").value;
+  var img = document.getElementById("Figure_3_test_image").value;
+  var fov = document.getElementById("Figure_3_field_of_view").value;
+  var num = document.getElementById("Figure_3_number_of_angles").value;
+  var filename = big_image_directory + "/figure_3/" + mth;
+  if ((mth === 'descan_line_') || (mth === 'rescan_line_')) {
+    filename = filename.concat(num);
+  }
+  filename = filename.concat(img + fov + res + ".mp4");
+  var vid = document.getElementById("Figure_3_vid");
+  vid.src = filename;
+}
